b73b4c8e199ebe4104efc7d037a2b724642893d8
CharllysOmena <charllysoliver@gmail.com>
2023-07-09 00:30:00 -0300

    teste unitario do homeService iniciado

--- src/app/modules/home/home.service.spec.ts
+++ src/app/modules/home/home.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HomeService } from './home.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environments';
 
-describe('HomeService', () => {
+fdescribe('HomeService', () => {
   let service: HomeService;
+  let http : HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(HomeService);
+    http = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be call the get metod with endpoint correct', () => {
+    const spy = spyOn(http, 'get').and.callThrough(); 
+    service.getCategorias();
+    expect(spy).toHaveBeenCalledOnceWith(environment.API_URL + "categories.php");
   });
 });
